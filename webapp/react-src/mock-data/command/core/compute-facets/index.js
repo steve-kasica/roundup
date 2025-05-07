7c1a31fb53483dc8ff3b580a8c8128ddab2331b5
Steve Kasica <kasica@cs.ubc.ca>
2025-05-07 13:29:18 -0500

    add more babyname-politics examples

--- webapp/react-src/mock-data/command/core/compute-facets/index.js
+++ webapp/react-src/mock-data/command/core/compute-facets/index.js
@@ -1,11 +1,17 @@
 import project1954092532131 from "./1954092532131";
-import project2469210019175 from "./2469210019175";
 import project2491520988186 from "./2491520988186";
 import project2646279702716 from "./2646279702716";
+import projectINLA from "./2620962230195";
+import projectNCNY from "./2469210019175";
+import projectHIIL from "./2631168748627";
+import projectAKGA from "./1777071525925";
 
 export default {
   1954092532131: project1954092532131,
-  2469210019175: project2469210019175,
   2491520988186: project2491520988186,
   2646279702716: project2646279702716,
+  2469210019175: projectNCNY,
+  2620962230195: projectINLA,
+  2631168748627: projectHIIL,
+  1777071525925: projectAKGA,
 };
