0a1c85e99e7bdf6e7b6ff6eeb65a4b1bb7be0ee3
Steve Kasica <kasica@cs.ubc.ca>
2025-05-12 11:21:26 -0500

    Add compute-facets mock data
    Adds compute-facets endpoint mock data for babyname politics projects,
    completing all columns in that example workflow

---
+++ webapp/react-src/mock-data/command/core/compute-facets/1899665756039/index.js
@@ -0,0 +1,13 @@
+import abbr from "./abbr.json";
+import gender from "./gender.json";
+import name from "./name.json";
+import value from "./value.json";
+import year from "./year.json";
+
+export default {
+  abbr,
+  gender,
+  name,
+  value,
+  year,
+};
--- webapp/react-src/mock-data/command/core/compute-facets/index.js
+++ webapp/react-src/mock-data/command/core/compute-facets/index.js
@@ -1,17 +1,25 @@
-import project1954092532131 from "./1954092532131";
-import project2491520988186 from "./2491520988186";
-import project2646279702716 from "./2646279702716";
-import projectINLA from "./2620962230195";
-import projectNCNY from "./2469210019175";
-import projectHIIL from "./2631168748627";
-import projectAKGA from "./1777071525925";
+// Importing this data via an alias creates a nice map of project IDs to project names
+import lambert2 from "./2491520988186";
+import lambert1 from "./2646279702716";
+import IN_LA from "./2620962230195";
+import NC_NY from "./2469210019175";
+import HI_IL from "./2631168748627";
+import AK_GA from "./1777071525925";
+import MA_MT from "./1954092532131";
+import UT_WY from "./1899665756039";
+import OH_TX from "./2429986839284";
 
 export default {
-  1954092532131: project1954092532131,
-  2491520988186: project2491520988186,
-  2646279702716: project2646279702716,
-  2469210019175: projectNCNY,
-  2620962230195: projectINLA,
-  2631168748627: projectHIIL,
-  1777071525925: projectAKGA,
+  // crime-and-heat projects
+  2646279702716: lambert1,
+  2491520988186: lambert2,
+
+  // baby name politices projects
+  2469210019175: NC_NY,
+  2620962230195: IN_LA,
+  2631168748627: HI_IL,
+  1777071525925: AK_GA,
+  1954092532131: MA_MT,
+  1899665756039: UT_WY,
+  2429986839284: OH_TX,
 };
