a079fb48aadc46712e4f745809b37498b21d32d1
Steve Kasica <kasica@cs.ubc.ca>
2025-05-16 11:19:01 -0500

    Parameterize some properties of AnimatedElipse

--- webapp/react-src/src/components/ui/AnimatedElipse.jsx
+++ webapp/react-src/src/components/ui/AnimatedElipse.jsx
@@ -1,28 +1,29 @@
 import { useState, useEffect } from "react";
 
-const AnimatedEllipsis = () => {
-    const [dots, setDots] = useState("");
-    const speed = 500;  // in ms
-    const elipseLength = 3;
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setDots((prev) => (prev.length < elipseLength ? prev + "." : ""));
-        }, speed);
+const AnimatedEllipsis = ({
+  speed = 250, // in ms
+  elipseLength = 3,
+}) => {
+  const [dots, setDots] = useState("");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < elipseLength ? prev + "." : ""));
+    }, speed);
 
-        return () => clearInterval(interval);
-    }, []);
-    return (
-        <span
-            style={{
-                display: "inline-block",
-                minWidth: `${elipseLength}ch`,
-                fontFamily: "monospace",
-                textAlign: "left"
-            }}
-        >
-            {dots}
-        </span>
-    );
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <span
+      style={{
+        display: "inline-block",
+        minWidth: `${elipseLength}ch`,
+        fontFamily: "monospace",
+        textAlign: "left",
+      }}
+    >
+      {dots}
+    </span>
+  );
 };
 
 export default AnimatedEllipsis;
