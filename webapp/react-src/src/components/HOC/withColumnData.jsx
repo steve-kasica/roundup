cefbbf6c047b04a5c223130954ffe83d518f9fb9
Steve Kasica <kasica@cs.ubc.ca>
2025-05-23 13:57:03 -0500

    Fix selection off-by-one-error

--- webapp/react-src/src/components/HOC/withColumnData.jsx
+++ webapp/react-src/src/components/HOC/withColumnData.jsx
@@ -145,7 +145,7 @@ export default function withColumnData(WrappedComponent) {
           if (!isNull && firstSelectedColumnId) {
             const anchorIdx = tableColumnIds.indexOf(firstSelectedColumnId);
             const [start, end] = [anchorIdx, index].sort((a, b) => a - b);
-            const selectedColumns = tableColumnIds.slice(start, end + 1);
+            const selectedColumns = tableColumnIds.slice(start, end);
             dispatch(setSelectedColumns(selectedColumns));
           }
         }}
