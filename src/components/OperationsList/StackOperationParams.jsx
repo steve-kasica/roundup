9769085e657c0aa7f81a2753fdd3fba3dd4d49c1
Steve Kasica <kasica@cs.ubc.ca>
2025-08-30 14:33:48 -0500

    Add stack parameters template

--- src/components/OperationsList/StackOperationParams.jsx
+++ src/components/OperationsList/StackOperationParams.jsx
@@ -1,9 +1,29 @@
-const StackOperationParams = () => {
+const StackOperationParams = ({
+  tables = ["customers", "orders", "products", "invoices", "shipments"],
+}) => {
   return (
     <div>
-      {/* Template content goes here */}
       <h2>Stack Operation Parameters</h2>
-      <p>This is a template for displaying Stack operation parameters.</p>
+      <p>
+        This component shows the list of table names that will be used by the
+        Stack operation. You can pass a `tables` prop (array of strings) to
+        override the default example list.
+      </p>
+
+      <section aria-label="table-names">
+        <h3>Tables</h3>
+        {tables && tables.length ? (
+          <ul>
+            {tables.map((t) => (
+              <li key={t}>{t}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>
+            <em>No tables provided.</em>
+          </p>
+        )}
+      </section>
     </div>
   );
 };
