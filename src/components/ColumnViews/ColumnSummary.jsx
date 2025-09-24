25e0b2953d2461f2fa8a083863146a6059dcd25c
Steve Kasica <kasica@cs.ubc.ca>
2025-09-24 13:50:03 -0500

    Update table high-level

--- src/components/ColumnViews/ColumnSummary.jsx
+++ src/components/ColumnViews/ColumnSummary.jsx
@@ -1,7 +1,18 @@
 /* eslint-disable react/prop-types */
 import withColumnData from "./withColumnData";
 import DescriptionList from "../ui/DescriptionList";
-import { Box, Stack, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Stack,
+  Typography,
+  styled,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { MoreVert } from "@mui/icons-material";
+import { useState, useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
 import { formatNumber } from "../../lib/utilities";
 
 const StyledDataTypeTypography = styled(Typography)(() => ({
@@ -18,19 +29,153 @@ const StyledStatLabel = styled(Typography)(() => ({
   textTransform: "capitalize", // Capitalizes first letter of each word
 }));
 
-const ColumnSummary = ({ column, nullCount, uniqueCount, completeCount }) => {
+const StyledHeaderContainer = styled(Box)(() => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  marginBottom: "8px",
+}));
+
+const DraggableColumnSummary = styled(Box)(
+  ({ theme, isDragging, isOver, canDropHere }) => ({
+    minWidth: 200,
+    padding: theme.spacing(1),
+    border: `2px solid ${
+      isOver && canDropHere
+        ? theme.palette.primary.main
+        : isDragging
+        ? theme.palette.action.disabled
+        : "transparent"
+    }`,
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor:
+      isOver && canDropHere
+        ? theme.palette.action.hover
+        : isDragging
+        ? theme.palette.action.selected
+        : "transparent",
+    cursor: isDragging ? "grabbing" : "grab",
+    opacity: isDragging ? 0.5 : 1,
+    transition: "all 0.2s ease",
+    "&:hover": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  })
+);
+
+const ColumnSummary = ({
+  column,
+  nullCount,
+  uniqueCount,
+  completeCount,
+  unselectColumn,
+  onDrop,
+  onDragEnd,
+}) => {
+  const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+  const ref = useRef(null);
+
+  // Calculate values first
+  const top = column?.max;
+  const tableIndex = column?.index ? column.index + 1 : 0;
+
+  // Drag functionality
+  const [{ isDragging }, drag] = useDrag({
+    type: "ColumnSummary",
+    item: () => ({
+      ...column,
+      type: "ColumnSummary",
+      tableIndex,
+    }),
+    canDrag: !!column,
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult();
+      if (onDragEnd) {
+        onDragEnd(item, dropResult, monitor.didDrop());
+      }
+    },
+  });
+
+  // Drop functionality
+  const [{ isOver, canDropHere }, drop] = useDrop({
+    accept: ["ColumnSummary", "ColumnCard"],
+    drop: (draggedItem, monitor) => {
+      // Prevent nested drops
+      if (monitor.didDrop()) {
+        return;
+      }
+
+      // Don't drop on self
+      if (draggedItem.id === column?.id) {
+        return;
+      }
+
+      const dropResult = onDrop ? onDrop(draggedItem, column, monitor) : {};
+      return { ...dropResult, droppedOn: column };
+    },
+    canDrop: (item) => item.id !== column?.id, // Can't drop on self
+    collect: (monitor) => ({
+      isOver: monitor.isOver({ shallow: true }),
+      canDropHere: monitor.canDrop(),
+    }),
+  });
+
+  // Combine drag and drop refs
+  const dragDropRef = (node) => {
+    ref.current = node;
+    drag(node);
+    drop(node);
+  };
+
   if (!column) {
     return <div>No column data available.</div>;
   }
 
-  const top = column.max;
-  const tableIndex = column.index + 1;
+  const handleMenuClick = (event) => {
+    event.stopPropagation();
+    setMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = (callback) => {
+    setMenuAnchorEl(null);
+    if (callback) callback();
+  };
 
   return (
-    <Box minWidth={200}>
-      <h3>
-        {tableIndex}. {column.name}
-      </h3>
+    <DraggableColumnSummary
+      ref={dragDropRef}
+      isDragging={isDragging}
+      isOver={isOver}
+      canDropHere={canDropHere}
+    >
+      <StyledHeaderContainer>
+        <h3 style={{ margin: 0, flex: 1 }}>
+          {tableIndex}. {column.name}
+        </h3>
+        <IconButton size="small" onClick={handleMenuClick} sx={{ p: 0.5 }}>
+          <MoreVert fontSize="small" />
+        </IconButton>
+      </StyledHeaderContainer>
+      <Menu
+        anchorEl={menuAnchorEl}
+        open={Boolean(menuAnchorEl)}
+        onClose={handleMenuClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem onClick={() => handleMenuClose(unselectColumn)}>
+          Deselect Column
+        </MenuItem>
+      </Menu>
       <Stack
         direction={"row"}
         spacing={2}
@@ -91,7 +236,7 @@ const ColumnSummary = ({ column, nullCount, uniqueCount, completeCount }) => {
           {top}
         </StyledDataTypeTypography>
       </Stack>
-    </Box>
+    </DraggableColumnSummary>
   );
 };
 
--- src/components/TableView/TableSummary.jsx
+++ src/components/TableView/TableSummary.jsx
@@ -1,23 +1,13 @@
 /* eslint-disable react/prop-types */
 
-import { Typography, Box, Chip, Card } from "@mui/material";
-import { Grid2 as Grid } from "@mui/material";
+import { Box, Card } from "@mui/material";
 import withTableData from "../HOC/withTableData";
-import { ColumnCard } from "../ColumnViews";
-import ColumnHeader from "../ColumnViews/ColumnHeader";
-import ColumnStats from "../ui/DescriptionList";
 import { ColumnSummary } from "../ColumnViews/ColumnSummary";
 
-const TableSummary = ({
-  table,
-  activeColumnIds,
-  columnCount,
-  isSelected,
-  isHovered,
-}) => {
+const TableSummary = ({ selectedColumnIds }) => {
   return (
     <Box
-      sx={{ p: 3 }}
+      p={1}
       display={"flex"}
       flexDirection="row"
       alignContent={"flex-start"}
@@ -26,7 +16,7 @@ const TableSummary = ({
       gap={1}
     >
       {/* Column Cards Grid */}
-      {table.columnIds.map((columnId) => (
+      {selectedColumnIds.map((columnId) => (
         <Card key={columnId} sx={{ p: 1, minWidth: 200, flex: "1 1 0" }}>
           <ColumnSummary id={columnId} />
         </Card>
