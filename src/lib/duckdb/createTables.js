f945b619d329deb77f2c129bd185d88de226aca4
Steve Kasica <kasica@cs.ubc.ca>
2026-02-14 14:15:00 -0600

    Adjust file import setting
    Another employment-discrimination modification.

--- src/lib/duckdb/createTables.js
+++ src/lib/duckdb/createTables.js
@@ -18,14 +18,7 @@
 
 // TODO: I might need to get more into sniff
 // https://motherduck.com/blog/taming-wild-csvs-with-duckdb-data-engineering/
-import { DUCKDB_TYPE_VARCHAR } from "./columnTypes";
 import { getDuckDB } from "./duckdbClient";
-import { SUPPORTED_TYPES } from "./columnTypes";
-
-// Remove VARCHAR from auto type candidates b/c its the default
-const AUTO_TYPE_CANDIDATES = SUPPORTED_TYPES.filter(
-  (type) => type !== DUCKDB_TYPE_VARCHAR,
-);
 
 export async function createTables(tableName, fileName) {
   const db = await getDuckDB();
@@ -37,7 +30,9 @@ export async function createTables(tableName, fileName) {
     `CREATE TABLE "${tableName}" AS 
     SELECT * FROM read_csv_auto('${fileName}', 
     HEADER=true,
-    ALL_VARCHAR=true);`,
+    ALL_VARCHAR=true,
+    ignore_errors=true,
+    null_padding=true);`,
   );
 
   await conn.close();
