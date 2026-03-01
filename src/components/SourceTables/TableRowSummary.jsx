58f5116ca881a25d513f2933236d9aafb7664c15
Steve Kasica <kasica@cs.ubc.ca>
2026-03-01 15:04:04 -0600

    Update select table styles

--- src/components/SourceTables/TableRowSummary.jsx
+++ src/components/SourceTables/TableRowSummary.jsx
@@ -26,7 +26,7 @@
  */
 
 import HighlightText from "../ui/HighlightText";
-import { Typography, Checkbox, Box } from "@mui/material";
+import { Typography, Checkbox, Box, TableRow } from "@mui/material";
 import { useMemo, useState } from "react";
 import { formatDate, formatBytes } from "../../lib/utilities";
 import { withTableData, withAssociatedAlerts } from "../HOC";
@@ -39,6 +39,7 @@ import {
 } from "../../slices/uiSlice";
 import { useSelector } from "react-redux";
 import StyledTableCell from "./StyledTableCell";
+import { RemoveRedEye } from "@mui/icons-material";
 
 export const TABLE_ROW_VIEW_CLASS = "TableRowSummary";
 
@@ -115,19 +116,29 @@ const TableRowSummary = ({
   };
 
   return (
-    <StyledTableRow
+    <TableRow
+      hover={!isDisabled || searchString.length === 0 || hasNameMatch}
+      selected={isSelected}
+      onClick={handleClick}
       isDragging={false} // TODO
-      isDisabled={isDisabled || (searchString.length > 0 && !hasNameMatch)}
-      isSelected={isSelected}
-      isFocused={isFocused}
-      isHovered={isHovered}
-      data-tableid={id}
-      data-selected={isSelected}
+      onDoubleClick={handleDoubleClick}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      onClick={handleClick}
-      onDoubleClick={handleDoubleClick}
+      // isDisabled={isDisabled || (searchString.length > 0 && !hasNameMatch)}
+      // isFocused={isFocused}
+      data-tableid={id}
+      data-selected={isSelected}
       ref={dragDropRef}
+      sx={{
+        userSelect: "none",
+        height: "1px", // allows the row to shrink to fit content, but not grow larger than its content
+        opacity:
+          isDisabled || (searchString.length > 0 && !hasNameMatch) ? 0.5 : 1,
+        cursor:
+          isDisabled || (searchString.length > 0 && !hasNameMatch)
+            ? "not-allowed"
+            : "pointer",
+      }}
     >
       <StyledTableCell
         sx={{
@@ -138,14 +149,16 @@ const TableRowSummary = ({
         <Box
           sx={{
             display: "flex",
+            flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
             cursor: "grab",
+            gap: 0.5,
+            height: "20px",
           }}
         >
           <Checkbox
             checked={isInSchema}
-            indeterminate={isSelected}
             size="small"
             disabled
             aria-label={`Select table ${name}`}
@@ -154,6 +167,13 @@ const TableRowSummary = ({
               pointerEvents: "none",
             }}
           />
+          {isFocused && (
+            <RemoveRedEye
+              sx={{
+                color: (theme) => theme.palette.action.active,
+              }}
+            ></RemoveRedEye>
+          )}
         </Box>
       </StyledTableCell>
       <StyledTableCell
@@ -254,7 +274,7 @@ const TableRowSummary = ({
           {formatDate(new Date(dateLastModified))}
         </Typography>
       </StyledTableCell>
-    </StyledTableRow>
+    </TableRow>
   );
 };
 
